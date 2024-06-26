c8eddcf7eabcfc1a3219576c8466c8930c9007cd
smontalbano <sdm030@gmail.com>
2024-06-25 23:23:15 -0400

    Fix game.win? method

--- lib/game.rb
+++ lib/game.rb
@@ -8,11 +8,11 @@ class Game
 
     def player_move(move, player)
         case move
-        when 7
+        when 1
             Board.mark_board(0, 0, player.sym)
-        when 8
+        when 2
             Board.mark_board(0, 2, player.sym)
-        when 9
+        when 3
             Board.mark_board(0, 4, player.sym)
         when 4
             Board.mark_board(2, 0, player.sym)
@@ -20,11 +20,11 @@ class Game
             Board.mark_board(2, 2, player.sym)
         when 6
             Board.mark_board(2, 4, player.sym)
-        when 1
+        when 7
             Board.mark_board(4, 0, player.sym)
-        when 2
+        when 8
             Board.mark_board(4, 2, player.sym)
-        when 3
+        when 9
             Board.mark_board(4, 4, player.sym)
         end
     end
@@ -36,9 +36,18 @@ class Game
             true
         elsif Board.get_board_value(4, 0) == Board.get_board_value(4, 2) && Board.get_board_value(4, 0) == Board.get_board_value(4, 4) then 
             true
+        elsif Board.get_board_value(0, 0) == Board.get_board_value(2, 0) && Board.get_board_value(0, 0) == Board.get_board_value(4, 0) then
+            true
+        elsif Board.get_board_value(0, 2) == Board.get_board_value(2, 2) && Board.get_board_value(0, 2) == Board.get_board_value(4, 2) then
+            true
+        elsif Board.get_board_value(0, 4) == Board.get_board_value(2, 4) && Board.get_board_value(0, 4) == Board.get_board_value(4, 4) then 
+            true
+        elsif Board.get_board_value(0, 0) == Board.get_board_value(2, 2) && Board.get_board_value(0, 0) == Board.get_board_value(4, 4) then
+            true
+        elsif Board.get_board_value(0, 4) == Board.get_board_value(2, 2) && Board.get_board_value(0, 4) == Board.get_board_value(4, 0) then
+            true
         else
             false
         end
     end
-
 end
