55a356985900b86e2554dd6c5b3b973498cf7356
smontalbano <sdm030@gmail.com>
2024-07-01 17:06:36 -0400

    Rewrite for readability and improvements to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,66 +1,67 @@
+# frozen_string_literal: true
+
+# The game class is an object with all the rules and
+# functions needed to play a game of Tic-Tac-Toe with
+# 2 players
+
 class Game
-    attr_reader :player_one, :player_two, :winner, :win, :tie
-    @win = false
-    @tie = false
-    @turn_count = 0
-    @sym_one = 'X'
-    @sym_two = 'O'
-    @@board = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    WINS = [ [0, 1, 2], [3, 4, 5], [6, 7, 8],       #Horizonatal Lines
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],                #Vertical Line
-    [0, 4, 8], [2, 4, 6]]                           #Diagonal Lines
+  attr_reader :player_one, :player_two, :winner, :win
 
-    def initialize(player_one_name, player_two_name)
-        @player_one = player_one_name
-        @player_two = player_two_name
-    end
+  @win = false
+  @turn_count = 0
+  @@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  WINS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], # Horizonatal Lines
+          [0, 3, 6], [1, 4, 7], [2, 5, 8], # Vertical Line
+          [0, 4, 8], [2, 4, 6]].freeze # Diagonal Lines
 
-    def turn(player)
-        player == @player_one ? player = @player_one : player = @player_two
-        player == @player_one ? sym = @sym_one : sym = @sym_two
-        player_input(player, sym)
-    end
-    
-    private
+  def initialize(player_one_name, player_two_name)
+    @player_one = player_one_name
+    @player_two = player_two_name
+  end
 
-    def player_input(player, sym)
-        self.display_board
-        puts "#{player}'s turn: "
-        move = gets.chomp.to_i
-        valid_move?(move) ? mark_square(player, move, sym) : invalid_move(player)
-    end
+  def turn(player)
+    player = player == @player_one ? @player_one : @player_two
+    sym = player == @player_one ? 'X' : 'O'
+    player_input(player, sym)
+  end
 
+  private
 
-    def valid_move?(sel)
-        sel.between?(1, 9) && @@board[sel] == ' ' ? true : false 
-    end
-    
-    def invalid_move(player)
-        puts "Invalid move. Input must be between 1 and 9 and cannot be an occupied square."
-        puts "Please try again."
-        turn(player)
-    end
-    
-    def self.mark_square(player, sel, sym)
-        @@board[sel - 1] = sym
-        @turn_count += 1
-        self.display_board
-        win?(player, sym)
-        self.tie? ? @tie = true : @tie = false
-    end
+  def player_input(player, sym)
+    display_board
+    puts "#{player}'s turn: "
+    move = gets.chomp.to_i
+    valid_move?(move) ? mark_square(player, move, sym) : invalid_move(player)
+  end
 
-    def win?(player, sym)
-        if WINS.any? { |line| line.all? { |idx| @@board[idx] == sym}} then
-            @win = true
-            @winner = player
-        end
-    end
+  def valid_move?(sel)
+    sel.between?(1, 9) && @@board[sel - 1] == ' ' ? true : false
+  end
 
-    def display_board
-        puts " #{@@board[0]} | #{@@board[1]} | #{@@board[2]} "
-        puts '-----------'
-        puts " #{@@board[3]} | #{@@board[4]} | #{@@board[5]} "
-        puts '-----------'
-        puts " #{@@board[6]} | #{@@board[7]} | #{@@board[8]} "
-    end
+  def invalid_move(player)
+    puts 'Invalid move. Input must be between 1 and 9 and cannot be an occupied square.'
+    puts 'Please try again.'
+    turn(player)
+  end
+
+  def mark_square(player, sel, sym)
+    @@board[sel - 1] = sym
+    display_board
+    win?(player, sym)
+  end
+
+  def win?(player, sym)
+    return unless WINS.any? { |line| line.all? { |idx| @@board[idx] == sym } }
+
+    @win = true
+    @winner = player
+  end
+
+  def display_board
+    puts " #{@@board[0]} | #{@@board[1]} | #{@@board[2]} "
+    puts '-----------'
+    puts " #{@@board[3]} | #{@@board[4]} | #{@@board[5]} "
+    puts '-----------'
+    puts " #{@@board[6]} | #{@@board[7]} | #{@@board[8]} "
+  end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,43 @@
-require_relative './lib/game.rb'
+# frozen_string_literal: true
 
-win = false 
-tie = false
+require 'pry-byebug'
+require_relative './lib/game'
+tie_count = 0
+puts "Enter player one's name: "
+player_one = gets.chomp
+puts "Enter player two's name: "
+player_two = gets.chomp
+game = Game.new(player_one, player_two)
+
+while tie_count < 9
+  game.turn(player_one)
+  tie_count += 1
+  if game.win
+    puts "#{game.winner} wins!"
+    break
+  elsif tie_count == 9
+    puts "It's a tie!"
+    break
+  end
+  game.turn(player_two)
+  tie_count += 1
+  if game.win
+    puts "#{game.winner} wins!"
+    break
+  elsif tie_count == 9
+    puts "It's a tie!"
+    break
+  end
+end
+
+# def tie_win?
+#   if game.win
+#     puts "#{game.winner} wins!"
+#     true
+#   elsif tie_count == 9
+#     puts "It's a tie!"
+#     true
+#   else
+#     false
+#   end
+# end
